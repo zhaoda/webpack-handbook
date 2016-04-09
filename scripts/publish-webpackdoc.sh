c99384db458733da7e77524184dd6c5c6352c0bc
zhaoda(赵达) <zd6437@gmail.com>
2016-04-10 01:53:39 +0800

    增加国内镜像

---
+++ scripts/publish-webpackdoc.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+cp -a content webpackdoc
+cd webpackdoc
+
+rm -rf _book
+rm -rf node_modules
+
+gitbook install
+
+git init
+
+git add .
+git commit -m "Publish to webpackdoc.com"
+
+git remote add origin ssh://webpackdoc.com/home/git/zhaoda/webpack-handbook.git
+
+git push --force origin master:master
+
+cd ..
+rm -rf webpackdoc
